974e64c67fd18a9fd266f40c5cc3cf313f510a62
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-22 21:01:06 +0300

    feat: use keyboard controls in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { Suspense, useMemo } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import { KeyboardControls } from "@react-three/drei";
+import { Experience } from "./components/Experience";
+import { Controls } from "./constants/controls";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const map = useMemo(
+    () => [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
+    ],
+    []
+  );
+  return (
+    <KeyboardControls map={map}>
+      <Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Suspense>
+          <Physics debug>
+            <Experience />
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </KeyboardControls>
+  );
 }
 
 export default App;
